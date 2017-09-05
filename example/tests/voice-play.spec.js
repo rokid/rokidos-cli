814da5c110ecc4e37d3537f79c1bbe9f34ff9b56
Yorkie Liu <yorkieliu@yorkie-on-rokid.local>
2017-09-06 01:12:34 +0800

    fix example

--- example/tests/voice-play.spec.js
+++ example/tests/voice-play.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 test('test voice play', (t) => {
-  t.send('若琪我要听歌', {
+  t.send('若琪我要听音乐', {
     "appId": "R233A4F187F34C94B93EE3BAECFCE2E3",
     "asr": "我要听音乐",
     "cloud": true,
@@ -12,11 +12,27 @@ test('test voice play', (t) => {
     "appId": "R233A4F187F34C94B93EE3BAECFCE2E3",
     "response": {
       "action": {
+        "form": "scene",
+        "media": {
+          "action": "PLAY",
+          "disableEvent": false,
+          "item": {
+            "itemId": "play_random$$ULT$$1245352",
+            "offsetInMilliseconds": 0,
+            "token": "play_random$$ULT$$1245352",
+            "type": "AUDIO",
+            "url": "https://music-proxy.rokid-inc.com/content/01/245/1245352-MP3-320K-FTD.mp3?sign=p8sDiQmqAYkZ9jR5wiWTSw8CAnFhPTEwMDM5ODY3Jms9QUtJRE1Fdm53SXdwNFlqUlU1NHhxd3VLQlRYMExOOWdJVFNRJmU9MTUwMzY0Njk1MSZ0PTE1MDM0NzQxNTEmcj0xOTU3OTgxMjMzJmY9L2NvbnRlbnQvMDEvMjQ1LzEyNDUzNTItTVAzLTMyMEstRlRELm1wMyZiPXVsdGltYXRl&transDeliveryCode=RK@21829674@1503474151@S"
+          }
+        },
         "shouldEndSession": false,
         "type": "NORMAL",
         "version": "2.0.0",
         "voice": {
-          "item": {}
+          "action": "PLAY",
+          "disableEvent": true,
+          "item": {
+            "tts": "音乐继续"
+          }
         }
       },
       "resType": "INTENT",
@@ -26,7 +42,7 @@ test('test voice play', (t) => {
     "startWithActiveWord": true,
     "version": "2.0.0"
   });
-  t.assert('tts', '音乐继续');
+  t.assert('voice', '音乐继续');
   t.assert('media.stop', true);
   // t.assert('media.play', );
 });
